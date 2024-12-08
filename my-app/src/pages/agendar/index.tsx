bb33935b89fc1198ca86b731b635cb5784224556
Aline Nunes <devenirseul@gmail.com>
2024-12-08 15:12:11 -0300

    Ajuste das telas de visualização de paciente e consulta

--- my-app/src/pages/agendar/index.tsx
+++ my-app/src/pages/agendar/index.tsx
@@ -2,52 +2,58 @@ import React from "react";
 import { useState } from "react";
 import { Style } from "./styles";
 import { TextInputMask } from 'react-native-masked-text';
-import { Picker } from '@react-native-picker/picker'
+import axios from 'axios';
 
 import {
     Text,
     View,
     TextInput,
     TouchableOpacity,
-    ScrollView
+    ScrollView,
+    Alert
 } from 'react-native';
 
 export default function Agendar(){
-    const [data, setData] = useState('');
-    const [hora, setHora] = useState('');
+    const [data_consulta, setData] = useState('');
+    const [doctor_id, setDoctor] = useState('');
+    const [paciente_id, setPaciente] = useState('');
 
+    const handleSubmit = async () => {
+         try { const response = await 
+            axios.post('http://localhost:/consultas/', {
+                 data_consulta, doctor_id: Number(doctor_id), paciente_id: Number(paciente_id),}); 
+                 if (response.status === 201) { 
+                    Alert.alert('Sucesso', 'Agendamento salvo com sucesso'); } } 
+                    catch (error) { 
+                        Alert.alert('Erro', 'Falha ao salvar agendamento'); } };
     return (
         <View style={Style.container}>
             <ScrollView style={Style.containerScroll}>
                 <View style={Style.box}>
                     
-                    <Text style={Style.titleText}>Nome Completo</Text>
-                    <TextInput style={Style.inputBox}></TextInput>
+                    <Text style={Style.titleText}>ID do paciente</Text>
+                    <TextInput style={Style.inputBox}
+                    value={paciente_id}
+                    onChangeText={setPaciente}></TextInput>
 
-                    <Text style={Style.titleText}>Médico</Text>
-                    <TextInput style={Style.inputBox}></TextInput>
+                    <Text style={Style.titleText}>ID do médico</Text>
+                    <TextInput style={Style.inputBox}
+                    value={doctor_id}
+                    onChangeText={setDoctor}></TextInput>
 
                     <Text style={Style.titleText}>Data</Text>
                     <View style={Style.inputBox}>
                             <TextInputMask type={'datetime'} options={{ 
                                 format: 'DD/MM/YYYY' }}
-                            value={data}
+                            value={data_consulta}
                             onChangeText={text => setData(text)}
                             placeholder="DD/MM/AAAA"
                             />
                     </View>
 
-                    <Text style={Style.titleText}>Horário</Text>
-                    <View style={Style.inputBox}>
-                            <TextInputMask type={'datetime'} options={{ 
-                                format: 'HH:MM' }}
-                            value={hora}
-                            onChangeText={text => setHora(text)}
-                            placeholder="--:--"
-                            />
-                    </View>
+        
 
-                    <TouchableOpacity style={Style.saveButton}>
+                    <TouchableOpacity style={Style.saveButton} onPress={handleSubmit}>
                         <Text style={Style.saveText}>Salvar</Text>
                     </TouchableOpacity>
                 </View>
